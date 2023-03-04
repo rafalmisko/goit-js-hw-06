1805a93683846d62ac894c35ed43092075b6bc3a
Rafał Miśkowiec <sbgrafo@gmail.com>
2023-03-04 10:51:14 +0100

    Initial commit

---
+++ js/task-01.js
@@ -0,0 +1,26 @@
+const countItemCategories = document.querySelectorAll(".item"); 
+console.log(`Number of categories: ${countItemCategories.length}`);
+
+
+//1. zdefinowana zm. 
+//2. metoda qsa spr. class item
+//3. consol z destruk. i policzeniem metodą length
+
+//nrCountItem.forEach()
+// Iterujący forEach
+// numbers.forEach(function (number, index) {
+//   console.log(`Indeks ${index}, wartość ${number}`);
+// });
+
+countItemCategories.forEach((item) => {
+		console.log(`Categories: ${item.firstElementChild.textContent}`);
+        console.log(`Elements: ${item.lastElementChild.children.length }`);
+	});
+
+
+//1. zm. the same
+//2. forEach, a-z
+//3. h2 is firstChild of the three
+//4. li is lastChild of the tree
+
+//. 
---
+++ js/task-02.js
@@ -0,0 +1,51 @@
+const ingredients = [
+  'Potatoes',
+  'Mushrooms',
+  'Garlic',
+  'Tomatos',
+  'Herbs',
+  'Condiments',
+];
+
+const newListItems = document.querySelector("#ingredients"); 
+
+const newItems = ingredients.map((item) => {
+ const newItem = document.createElement("li"); 
+ newItem.textContent = item; 
+ newItem.classList.add("item");
+  
+ return newItem;
+});
+
+newListItems.append(...newItems);
+
+
+ 
+//map create new tab, forEach 0
+// tablica.map((element, index, array) => {
+   // Ciało funkcji zwrotnej
+// });
+
+// const newList = document.querySelector("#ingredients");
+// const firstItem = document.createElement("li");
+// firstItem.textContent = "Potatoes";
+// newList.prepend(firstItem);
+// const secondItem = document.createElement("li");
+// secondItem.textContent = "Mushrooms";
+// newList.after(firstItem);
+// console.log(newList);
+
+
+
+
+// newList.forEach((item) => {
+//  const newItem = document.createElement("li"); 
+//  newItem.textContent = ingredients; 
+//  newItem.classList.add("items");
+//  newList.appendChild(newItem); 
+//  return newItem;
+// });
+
+// newList.append(...newItem);
+
+ 
