d47ea78cb8ea1306ada0e1bc272155d0f8155175
Rafał Miśkowiec <sbgrafo@gmail.com>
2023-03-04 10:53:56 +0100

    Update task-01.js

--- js/task-01.js
+++ js/task-01.js
@@ -2,6 +2,12 @@ const countItemCategories = document.querySelectorAll(".item");
 console.log(`Number of categories: ${countItemCategories.length}`);
 
 
+
+countItemCategories.forEach((item) => {
+		console.log(`Categories: ${item.firstElementChild.textContent}`);
+        console.log(`Elements: ${item.lastElementChild.children.length }`);
+	});
+
 //1. zdefinowana zm. 
 //2. metoda qsa spr. class item
 //3. consol z destruk. i policzeniem metodą length
@@ -11,12 +17,6 @@ console.log(`Number of categories: ${countItemCategories.length}`);
 // numbers.forEach(function (number, index) {
 //   console.log(`Indeks ${index}, wartość ${number}`);
 // });
-
-countItemCategories.forEach((item) => {
-		console.log(`Categories: ${item.firstElementChild.textContent}`);
-        console.log(`Elements: ${item.lastElementChild.children.length }`);
-	});
-
 
 //1. zm. the same
 //2. forEach, a-z
